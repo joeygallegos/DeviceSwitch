41c7b21d7f72eafea48c5bc8e1c9068f2f3b35ae
Joey Gallegos <joey@joeygallegos.com>
2023-06-05 18:27:05 -0500

    add toast notification

--- DeviceSwitch.ps1
+++ DeviceSwitch.ps1
@@ -11,6 +11,7 @@ Import-Module -Name AudioDeviceCmdlets
 
 # For message box
 Add-Type -AssemblyName PresentationFramework
+Import-Module BurntToast
 
 # Get list of Audio Devices and filter by Playback and Default
 $CurrentDevice = Get-AudioDevice -List | Where-Object Type -like "Playback" | Where-Object Default -eq $true
@@ -23,13 +24,16 @@ if ($CurrentDevice -eq $null) {
     [System.Windows.MessageBox]::Show('The current audio output device is not TV or Headset', 'DeviceSwitchUtil', 'Ok', 'Error')
 }
 else {
+    $DesiredDevicePrettyName = "";
     if ($CurrentDevice.Name -like "*$TelevisionDriver*") {
         # Switch to HeadsetDriver
         $DesiredDevice = Get-AudioDevice -List | Where-Object Type -like "Playback" | Where-Object Name -like "*$HeadsetDriver*"
+        $DesiredDevicePrettyName = "Headset"
     }
     else {
         # Switch to TelevisionDriver
         $DesiredDevice = Get-AudioDevice -List | Where-Object Type -like "Playback" | Where-Object Name -like "*$TelevisionDriver*"
+        $DesiredDevicePrettyName = "TV"
     }
 
     if ($DesiredDevice -eq $null) {
@@ -40,5 +44,6 @@ else {
         # Set the desired device as the default audio output device
         Set-AudioDevice -ID $DesiredDevice.ID
         Write-Host "Audio output device changed to '$($DesiredDevice.Name)'."
+        New-BurntToastNotification -Text "Audio output device changed to '$($DesiredDevicePrettyName)'." -Sound "SMS" -AppLogo C:\none.jpg
     }
 }
